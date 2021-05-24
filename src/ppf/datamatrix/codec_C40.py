2c5ffebde2b177b051311797f0f590281d47a715
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2021-05-24 22:56:22 +0200

    Refactoring text modes ++++++++++++++++++
    Encoding and decoding routines of C40, TEXT, X12
    are very similar => refactored to share code (DRY)

--- src/ppf/datamatrix/codec_C40.py
+++ src/ppf/datamatrix/codec_C40.py
@@ -13,8 +13,8 @@ decoded = encoded.decode('datamatrix.C40')
 """
 
 import codecs
-from .codec_common import set1, set2, add_inverse_lookup, tokenize
-from .codec_common import pack_words, unpack_words
+from .codec_common import set1, set2, add_inverse_lookup
+from .codec_common import encode_text_mode, decode_text_mode
 
 # Source: https://en.m.wikipedia.org/wiki/Data_Matrix
 set0 = ' 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -33,63 +33,17 @@ add_inverse_lookup(codepage)
 
 def encode_to_C40(msg):
     """Encode to datamatrix.C40."""
-    # Recipe:
-    # 1. Declace C40: b'\xE6'
-    # 2. Convert chars in msg to raw-bytes by translation via codepage
-    # 3. Pack 3 raw-bytes into 2 encoded bytes (words)
-    # 4. Handle remaining chars in msg: Switch to ascii ('\xFE'), encode
-    #    remaining chars to datamatrix.ascii
-    #
-    # Important special case:
-    # C40 uses multiple sets (see above). Default is set0 but set0 has
-    # codes to switch to set1, set2, or set3 for the following code.
-    # Example: 'a' => [2, 1], where 2 is the 'switch to set3' code and 1
-    # is the code for 'a' in set3.
-    # When packing raw-bytes into words, it can happen that the 'switch
-    # to setX' and the code after it are packed into separate words.
-    # In particular, it may happen that the 'switch to setX' code is packed
-    # into the *last* word and the code after it is *not* packed into a word
-    # (because there are <3 raw bytes remaining). In that case, we *do*
-    # pack the 'switch to setX' into the last word. It will be immediately
-    # followed by the 'switch to ascii' code, anyway. We have to make sure
-    # that the character "split in half" is added to the "ascii tail".
-
-    raw = sum([codepage[char] for char in msg], [])
-
-    n_words = len(raw) // 3
-    topack = raw[:n_words * 3]
-    toascii = msg[len(list(tokenize(topack))):]
-
-    enc = b'\xE6' + pack_words(topack)
-    if len(toascii) > 0:
-        enc += b'\xFE'
-        enc += toascii.encode('datamatrix.ascii')
-
-    return enc, len(enc)
+    return encode_text_mode(msg, codepage, b'\xE6', True)
 
 
 def decode_from_C40(enc):
     """Decode datamatrix.C40-encoded message."""
-    enc = bytes(enc)
-    if enc[0] != 0xE6:
-        raise ValueError('f{enc} is not valid C40 code')
+    try:
+        msg, length = decode_text_mode(enc, codepage, b'\xE6', True)
+    except ValueError:
+        raise ValueError(f'{enc} is not valid C40 code')
 
-    enc = enc[1:]
-    # if there is an b'\xFE' at an even position, it means 'switch to ASCII'
-    # note: in an odd position, it is just a coincidence to word packing.
-    pos = enc[::2].find(b'\xFE')
-    if pos > -1:
-        words = enc[: 2 * pos]
-        remainder = enc[2 * pos + 1:]
-    else:
-        words = enc
-        remainder = b''
-
-    raw = unpack_words(words)
-    msg = ''.join([codepage[code] for code in tokenize(raw)])
-    msg += remainder.decode('datamatrix.ascii')
-
-    return msg, len(msg)
+    return msg, length
 
 
 def search_codec_C40(encoding_name):
--- src/ppf/datamatrix/codec_common.py
+++ src/ppf/datamatrix/codec_common.py
@@ -20,6 +20,77 @@ def add_inverse_lookup(codepage):
         codepage[bytes(code)] = char
 
 
+def encode_text_mode(msg, codepage, magic, multiset):
+    """Encode to datamatrix.C40."""
+    # Recipe:
+    # 1. Declace codepage by magic code (e.g., b'\xE6' for C40)
+    # 2. Convert chars in msg to raw-bytes by translation via codepage
+    # 3. Pack 3 raw-bytes into 2 encoded bytes (words)
+    # 4. Handle remaining chars in msg: Switch to ascii ('\xFE'), encode
+    #    remaining chars to datamatrix.ascii
+    #
+    # Important special case:
+    # C40 (e.g.) uses multiple sets (see above). Default is set0 but set0
+    # has codes to switch to set1, set2, or set3 for the following code.
+    # Example: 'a' => [2, 1], where 2 is the 'switch to set3' code and 1
+    # is the code for 'a' in set3.
+    # When packing raw-bytes into words, it can happen that the 'switch
+    # to setX' and the code after it are packed into separate words.
+    # In particular, it may happen that the 'switch to setX' code is packed
+    # into the *last* word and the code after it is *not* packed into a word
+    # (because there are <3 raw bytes remaining). In that case, we *do*
+    # pack the 'switch to setX' into the last word. It will be immediately
+    # followed by the 'switch to ascii' code, anyway. We have to make sure
+    # that the character "split in half" is added to the "ascii tail".
+
+    try:
+        raw = sum([codepage[char] for char in msg], [])
+    except KeyError:
+        raise ValueError('Incompatible character in msg')
+
+    n_words = len(raw) // 3
+    topack = raw[:n_words * 3]
+    if multiset:
+        toascii = msg[len(list(tokenize(topack))):]
+    else:
+        toascii = msg[len(topack):]
+
+    enc = magic + pack_words(topack)
+    if len(toascii) > 0:
+        enc += b'\xFE'
+        enc += toascii.encode('datamatrix.ascii')
+
+    return enc, len(enc)
+
+
+def decode_text_mode(enc, codepage, magic, multiset):
+    """Decode datamatrix.C40-encoded message."""
+    enc = bytes(enc)
+    magic = list(magic)[0]
+    if enc[0] != magic:
+        raise ValueError()
+
+    enc = enc[1:]
+    # if there is an b'\xFE' at an even position, it means 'switch to ASCII'
+    # note: in an odd position, it is just a coincidence to word packing.
+    pos = enc[::2].find(b'\xFE')
+    if pos > -1:
+        words = enc[: 2 * pos]
+        remainder = enc[2 * pos + 1:]
+    else:
+        words = enc
+        remainder = b''
+
+    raw = unpack_words(words)
+    if multiset:
+        msg = ''.join([codepage[code] for code in tokenize(raw)])
+    else:
+        msg = ''.join([codepage[bytes([code])] for code in raw])
+    msg += remainder.decode('datamatrix.ascii')
+
+    return msg, len(msg)
+
+
 def pack_words(raw):
     """3 raw bytes to 2 encoded bytes stuffing for datamatrix text modes."""
     if len(raw) % 3 != 0:
