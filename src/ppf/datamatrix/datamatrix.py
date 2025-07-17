d30ec6e11ddd151d70a0158cc1efd2a591bb719b
yatesdr <derek.yates@live.com>
2025-07-16 22:55:38 -0400

    Update datamatrix.py

--- src/ppf/datamatrix/datamatrix.py
+++ src/ppf/datamatrix/datamatrix.py
@@ -22,8 +22,8 @@ svg_rects_template = \
     'style="background-color:{bg}" ' \
     'xmlns="http://www.w3.org/2000/svg" ' \
     'xmlns:ev="http://www.w3.org/2001/xml-events" ' \
-    'xmlns:xlink="http://www.w3.org/1999/xlink"' \
-    'transform="scale({x_scale},{y_scale}) >' \
+    'xmlns:xlink="http://www.w3.org/1999/xlink" ' \
+    'transform="scale({x_scale},{y_scale})">' \
     '{rects}' \
     '</svg>'
 
