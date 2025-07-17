c9b4852ce07f12387d16f516e0eb1e57b801b91c
yatesdr <derek.yates@live.com>
2025-07-16 21:02:30 -0400

    Update datamatrix.py

--- src/ppf/datamatrix/datamatrix.py
+++ src/ppf/datamatrix/datamatrix.py
@@ -85,9 +85,9 @@ class DataMatrix():
         for i,line in enumerate(mat):    
             for j,sym in enumerate(line):
                 if sym:
-                    yield f"<rect width=1 height=1 x='{i+1}' y='{j+1}' fill='{fg}' stroke='{fg}'>\n"
+                    yield f"<rect width='1' height='1' x='{i+1}' y='{j+1}' fill='{fg}' stroke='{fg}'>\n"
                 else:
-                    yield f"<rect width=1 height=1 x='{i+1}' y='{j+1}' fill='{bg}' stroke='{bg}'>\n"
+                    yield f"<rect width='1' height='1' x='{i+1}' y='{j+1}' fill='{bg}' stroke='{bg}'>\n"
 
     def svg(self, fg='#000', bg='#FFF', margin=1):
         """
